111360a2942ada8ce216358a9854832e2c64641e
rcarhart <rcarhart2@gmail.com>
2024-02-04 19:29:23 -0500

    contact form custom redirect

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,5 @@
+
+const result = document.getElementById('result');
 document.addEventListener('DOMContentLoaded', function() {
   // ... h3 animation code ...
 
@@ -44,17 +46,53 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 //-------hcatpcha-------//
+
+
+// form.addEventListener('submit', function(e) {
+
+//     const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
+
+//     if (!hCaptcha) {
+//         e.preventDefault();
+//         alert("Please fill out captcha field")
+//         return
+//     }
+// });
+//------Form Submission------//
+
 const form = document.getElementById('form');
-
 form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
 
-    const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
-
-    if (!hCaptcha) {
-        e.preventDefault();
-        alert("Please fill out captcha field")
-        return
-    }
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
-
-
